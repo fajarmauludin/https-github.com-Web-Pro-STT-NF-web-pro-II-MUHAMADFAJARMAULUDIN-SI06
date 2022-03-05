f92a8fe5ef7310c4780b475fef88eeb808627be0
fajarmauludin <100998100+fajarmauludin@users.noreply.github.com>
2022-03-05 18:18:46 +0700

    Add files via upload

---
+++ array_push.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+    $tims = ["Irfan", "Domami", "Algifari", "Gilang"];
+    array_push($tims, "indra");
+    foreach($tims as $person) {
+        echo $person. '<br/>';
+    }
+?>
+<body>
+</html>
